6e4eca22886203f6b609dc6952b6a3b698fdfa1b
Aken <arinov.e@yandex.ru>
2023-12-16 13:31:32 +0300

    Fix typo

--- src/getParamsFromReadline.ts
+++ src/getParamsFromReadline.ts
@@ -4,8 +4,8 @@ import { stdin as input, stdout as output } from 'node:process';
 export const getParamsFromReadline = async () => {
   const rl = readline.createInterface({ input, output });
 
-  const year = await rl.question('Enter a year ');
-  const month = await rl.question('Enter a moth ');
+  const year = await rl.question('Enter a year: ');
+  const month = await rl.question('Enter a month: ');
 
   rl.close();
 
